d3f16b79381d370cfe0b3a63c08926a28f09dfd2
Liang Ding <d@b3log.org>
2021-02-08 00:45:21 +0800

    :art: 写共享存储申请权限

--- app/src/main/java/org/b3log/siyuan/MainActivity.java
+++ app/src/main/java/org/b3log/siyuan/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
                     // granted
                 } else {
-                    onDestroy();
+                   System.exit(-1);
                 }
                 return;
             }
