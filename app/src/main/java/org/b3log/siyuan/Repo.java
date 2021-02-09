0dad8b99c0f72a703f27987e36a468cf19b4346b
Liang Ding <d@b3log.org>
2021-02-09 18:14:01 +0800

    :art: repo sync

--- app/src/main/java/org/b3log/siyuan/Repo.java
+++ app/src/main/java/org/b3log/siyuan/Repo.java
@@ -6,7 +6,19 @@ import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.util.FS;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -41,8 +53,7 @@ public final class Repo {
 
                 final String localPath = box.optString("path");
 
-
-                // pull
+                pull(localPath, keyFile);
 
                 Androidk.reloadBox(localPath);
 
@@ -56,6 +67,30 @@ public final class Repo {
         } catch (final Exception e) {
             Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private static void pull(final String localPath, final String keyFile) throws Exception {
+        final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+            @Override
+            protected void configure(final OpenSshConfig.Host host, final Session session) {
+            }
+
+            @Override
+            protected JSch createDefaultJSch(final FS fs) throws JSchException {
+                final JSch defaultJSch = super.createDefaultJSch(fs);
+                defaultJSch.addIdentity(keyFile);
+                return defaultJSch;
+            }
+        };
+
+        final Git repo = Git.open(new File(localPath));
+        final PullCommand pullCommand = repo.pull();
+        pullCommand.setTransportConfigCallback(transport -> {
+            SshTransport sshTransport = (SshTransport) transport;
+            sshTransport.setSshSessionFactory(sshSessionFactory);
+        });
+        final PullResult pullResult = pullCommand.call();
+        Log.i("", pullResult.toString());
     }
 
 }
