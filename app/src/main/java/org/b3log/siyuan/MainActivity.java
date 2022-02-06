fa144ebdf3b48392211d69edfe8bb93afe95fcc6
Liang Ding <d@b3log.org>
2022-02-06 20:24:54 +0800

    :art: Android 端状态栏适配当前主题模式 https://github.com/siyuan-note/siyuan/issues/3998

--- app/src/main/java/org/b3log/siyuan/MainActivity.java
+++ app/src/main/java/org/b3log/siyuan/MainActivity.java
@@ -7,7 +7,6 @@
 package org.b3log.siyuan;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
@@ -23,7 +22,6 @@ import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -74,9 +72,17 @@ public class MainActivity extends AppCompatActivity {
         if (null != actionBar) {
             actionBar.hide();
         }
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         getWindow().setNavigationBarColor(Color.TRANSPARENT);
-        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
         }
@@ -341,16 +347,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return ret;
-    }
-
-    public static void setWindowFlag(final Activity activity, final int bits, boolean on) {
-        final Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
     }
 }
