8316a17eb910747954527fa0d35f5682369b2c1d
Daniel <845765@qq.com>
2024-04-13 10:36:10 +0800

    :art: 调整了构建产物命名 https://github.com/siyuan-note/siyuan-android/pull/14

--- app/src/main/java/org/b3log/siyuan/DebugModeChecker.java
+++ app/src/main/java/org/b3log/siyuan/DebugModeChecker.java
@@ -1,30 +1,54 @@
+/*
+ * SiYuan - 源于思考，饮水思源
+ * Copyright (c) 2020-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.b3log.siyuan;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 
+/**
+ * Debug mode checker.
+ *
+ * @author <a href="https://github.com/wwxiaoqi">Jane Haring</a>
+ * @version 1.0.0.0, Apr 13, 2024
+ * @since 3.0.10
+ */
 public class DebugModeChecker {
 
-  /**
-   * Checks if the current package name contains ".debug" and if debug mode is enabled.
-   *
-   * @param context The Android context used to retrieve the package information.
-   * @return true if the package name contains ".debug" and debug mode is enabled, false otherwise.
-   */
-  public static boolean isDebugPackageAndMode(Context context) {
-    PackageManager packageManager = context.getPackageManager();
-    ApplicationInfo appInfo = null;
-    try {
-      appInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
-    } catch (PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
-    }
+    /**
+     * Checks if the current package name contains ".debug" and if debug mode is enabled.
+     *
+     * @param context The Android context used to retrieve the package information.
+     * @return true if the package name contains ".debug" and debug mode is enabled, false otherwise.
+     */
+    public static boolean isDebugPackageAndMode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    // Check if the package name contains ".debug"
-    boolean isDebugPackage = context.getPackageName() != null && context.getPackageName().contains(".debug");
-    boolean isDebugMode = appInfo != null && (appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-    return isDebugPackage && isDebugMode;
-  }
+        // Check if the package name contains ".debug"
+        boolean isDebugPackage = context.getPackageName() != null && context.getPackageName().contains(".debug");
+        boolean isDebugMode = appInfo != null && (appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        return isDebugPackage && isDebugMode;
+    }
 
 }
