af553110850370e651d474c596f96ffffdc0269e
Daniel <845765@qq.com>
2025-03-28 19:43:48 +0800

    :art: Supports local shorthands on Android https://github.com/siyuan-note/siyuan/issues/14414

--- app/src/main/java/org/b3log/siyuan/ShortcutActivity.java
+++ app/src/main/java/org/b3log/siyuan/ShortcutActivity.java
@@ -17,7 +17,6 @@
  */
 package org.b3log.siyuan;
 
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.ShortcutInfo;
