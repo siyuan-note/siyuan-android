6e077b66ee0766ee00c647d59babef379ab53a40
Joshua Su <i@joshua.su>
2026-03-04 23:34:16 +0800

    Patch missing RedirectUriReceiverActivity for commit  (https://github.com/siyuan-note/siyuan-android/pull/25)

---
+++ app/src/main/java/org/b3log/siyuan/RedirectUriReceiverActivity.java
@@ -0,0 +1,68 @@
+/*
+ * SiYuan - 源于思考，饮水思源
+ * Copyright (c) 2020-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.siyuan;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * OIDC redirect receiver activity.
+ *
+ * @author <a href="https://88250.b3log.org">Liang Ding</a>
+ * @version 1.0.0.0, Jan 30, 2026
+ * @since 1.0.0
+ */
+public class RedirectUriReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleIntent(getIntent());
+
+    }
+
+    @Override
+    protected void onNewIntent(final Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(final Intent intent) {
+        if (null == intent) {
+            return;
+        }
+
+        final Uri data = intent.getData();
+        if (null == data) {
+            return;
+        }
+
+        final Intent target = new Intent(this, MainActivity.class);
+        target.putExtra("oidcCallback", data.toString());
+        target.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        startActivity(target);
+
+        finish();
+        // Prevent any transition flicker for this transparent one-shot activity.
+        overridePendingTransition(0, 0);
+    }
+}
